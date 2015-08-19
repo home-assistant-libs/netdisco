e0d05a87e482f829898081a4c943a4c4daabffb2
Paulus Schoutsen <paulus.schoutsen@mycase.com>
2015-08-19 13:50:08 -0700

    Fix setup.py - thanks @tomduijf

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='netdisco',
-      version='0.1',
+      version='0.2',
       description='Discover devices on your local network',
       url='http://github.com/balloob/netdisco',
       author='Paulus Schoutsen',
       author_email='Paulus@PaulusSchoutsen.nl',
       license='MIT',
       install_requires=['requests>=2.0', 'zeroconf>=0.17.2'],
-      packages=['netdisco'],
+      packages=find_packages(),
       zip_safe=False)
