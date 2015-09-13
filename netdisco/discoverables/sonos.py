7f111e81e50a65271ce528eb991f16c6a5f218a8
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-09-12 18:57:56 -0700

    Discover Sonos

---
+++ netdisco/discoverables/sonos.py
@@ -0,0 +1,12 @@
+""" Discovers Sonos devices. """
+
+from . import SSDPDiscoverable
+
+
+# pylint: disable=too-few-public-methods
+class Discoverable(SSDPDiscoverable):
+    """ Adds support for discovering Sonos devices. """
+
+    def get_entries(self):
+        """ Get all the Sonos device uPnP entries. """
+        return self.find_by_st("urn:schemas-upnp-org:device:ZonePlayer:1")
