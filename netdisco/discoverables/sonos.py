21fe7dc5e4e4f912d00e42e48a6a663dac6eedc4
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-09-13 07:37:31 -0700

    Sonos info more usable

--- netdisco/discoverables/sonos.py
+++ netdisco/discoverables/sonos.py
@@ -2,10 +2,19 @@
 
 from . import SSDPDiscoverable
 
+try:
+    from urlparse import urlparse  # Py2
+except ImportError:
+    from urllib.parse import urlparse  # Py3
+
 
 # pylint: disable=too-few-public-methods
 class Discoverable(SSDPDiscoverable):
     """ Adds support for discovering Sonos devices. """
+
+    def info_from_entry(self, entry):
+        """ Returns the most important info from a uPnP entry. """
+        return urlparse(entry.values['location']).hostname
 
     def get_entries(self):
         """ Get all the Sonos device uPnP entries. """
