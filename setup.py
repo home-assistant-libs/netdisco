a3b71e16f2ced748af950114d503fa7962d79685
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2015-10-02 06:44:20 -0700

    Version bump for requirements upgrade

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='netdisco',
-      version='0.4.1',
+      version='0.4.2',
       description='Discover devices on your local network',
       url='http://github.com/balloob/netdisco',
       author='Paulus Schoutsen',
       author_email='Paulus@PaulusSchoutsen.nl',
       license='MIT',
-      install_requires=['requests>=2.0', 'zeroconf>=0.17.2'],
+      install_requires=['requests>=2.0', 'zeroconf>=0.17.4'],
       packages=find_packages(),
       zip_safe=False)
