dbbe3c41d049fa2f69446973b365d8d9f007c09e
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-04-09 20:23:56 -0700

    Version bump to 0.6.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='netdisco',
-      version='0.6.1',
+      version='0.6.2',
       description='Discover devices on your local network',
       url='http://github.com/balloob/netdisco',
       author='Paulus Schoutsen',
