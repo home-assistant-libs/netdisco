ce7e5d0ad7376c0ef9deef1cf77c5e1741c0ac3d
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2016-04-10 18:03:05 -0700

    Update dependency zeroconf to 0.17.5 + version bump

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='netdisco',
-      version='0.6.2',
+      version='0.6.3',
       description='Discover devices on your local network',
       url='http://github.com/balloob/netdisco',
       author='Paulus Schoutsen',
       author_email='Paulus@PaulusSchoutsen.nl',
       license='MIT',
-      install_requires=['netifaces>=0.10.0', 'requests>=2.0', 'zeroconf>=0.17.4'],
+      install_requires=['netifaces>=0.10.0', 'requests>=2.0', 'zeroconf>=0.17.5'],
       packages=find_packages(),
       zip_safe=False)
