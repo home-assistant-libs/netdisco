a0b3e325240ab90ff27d11b6951907c7a0917f75
Brent <bah2830@users.noreply.github.com>
2016-05-10 01:42:28 -0500

    Added support for roku (#31)

---
+++ netdisco/discoverables/roku.py
@@ -0,0 +1,17 @@
+"""Discovers Roku players."""
+
+from netdisco.util import urlparse
+from . import SSDPDiscoverable
+
+
+class Discoverable(SSDPDiscoverable):
+    """Adds support for discovering Roku media players."""
+
+    def info_from_entry(self, entry):
+        """Returns the most important info from a uPnP entry."""
+        info = urlparse(entry.location)
+        return info.hostname, info.port
+
+    def get_entries(self):
+        """Get all the Roku entries."""
+        return self.find_by_st("roku:ecp")
