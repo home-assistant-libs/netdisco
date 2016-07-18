926808fa9263c1909084216d18fef4ed4c60ad28
Cameron Bulock <cameron@bulock.com>
2016-07-18 21:06:31 +0000

    add support for detecting DirecTV receivers

---
+++ netdisco/discoverables/directv.py
@@ -0,0 +1,20 @@
+"""Discover DirecTV Receivers."""
+from netdisco.util import urlparse
+from . import SSDPDiscoverable
+
+
+class Discoverable(SSDPDiscoverable):
+    """Add support for discovering DirecTV Receivers."""
+
+    def info_from_entry(self, entry):
+        """Return the most important info from a uPnP entry."""
+        url = urlparse(entry.values['location'])
+
+        return url.hostname
+
+    def get_entries(self):
+        """Get all the DirecTV uPnP entries."""
+        return self.find_by_device_description({
+            "manufacturer": "DIRECTV",
+            "deviceType": "urn:schemas-upnp-org:device:MediaServer:1"
+        })
