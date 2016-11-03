b483ee4ef91e9d278962990a59c1d86865ec83d3
sfam <sfam21@gmail.com>
2016-11-03 18:43:11 +0000

    Add support for Samsung Smart Air Conditioner

---
+++ netdisco/discoverables/samsungac.py
@@ -0,0 +1,14 @@
+"""Discover Samsung Smart AC devices."""
+from . import BaseDiscoverable
+
+
+class Discoverable(BaseDiscoverable):
+    """Add support for discovering a Samsung Smart AC device."""
+
+    def __init__(self, netdis):
+        """Initialize the Samsung Smart AC discovery."""
+        self._netdis = netdis
+
+    def get_entries(self):
+        """Get all the Samsung Smart AC details."""
+        return self._netdis.samsungac.entries
