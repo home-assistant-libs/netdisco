c106816c51429a543af29298e0bea99d5a0d6241
Oliver <scarface-4711@users.noreply.github.com>
2016-12-12 05:55:34 +0100

    Added support for Denon AVR receivers (#74)
    * Added support for Denon AVR receivers
    
    * Correcting description

---
+++ netdisco/discoverables/denonavr.py
@@ -0,0 +1,30 @@
+"""Discover Denon AVR devices."""
+
+try:
+    # python 3
+    from urllib.parse import urlparse
+except ImportError:
+    # python 2
+    from urlparse import urlparse
+
+from . import SSDPDiscoverable
+
+
+class Discoverable(SSDPDiscoverable):
+    """Add support for discovering Denon AVR devices."""
+
+    def get_entries(self):
+        """Get all Denon AVR uPnP entries."""
+        return self.find_by_device_description({
+            "manufacturer": "Denon",
+            "deviceType": "urn:schemas-upnp-org:device:MediaRenderer:1"
+        })
+
+    def info_from_entry(self, entry):
+        """Get most important info, which is name, model and host."""
+        name = entry.description['device']['friendlyName']
+        model = entry.description['device']['modelName']
+        host = urlparse(
+            entry.description['device']['presentationURL']).hostname
+
+        return (host, name, model)
