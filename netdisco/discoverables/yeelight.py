38228924e3a26440f2a3dd2f036fe8f3fd7c1855
Jared J <jjensn@users.noreply.github.com>
2017-01-05 02:45:05 -0500

    Yeelight support (#81)
    * Add yeelight support
    
    * Yeelight: return information as a dict
    
    Currently returning host, port, hostname, device_type (rgb, white) and mdns properties (mac addr and epoch from bulbs)
    
    * Fixing linting isues

---
+++ netdisco/discoverables/yeelight.py
@@ -0,0 +1,32 @@
+"""Discover Yeelight bulbs, based on Kodi discoverable."""
+import logging
+from . import MDNSDiscoverable
+
+
+# pylint: disable=too-few-public-methods
+class Discoverable(MDNSDiscoverable):
+    """Add support for discovering Yeelight."""
+
+    def __init__(self, nd):
+        """Initialize the Yeelight discovery."""
+        super(Discoverable, self).__init__(nd, '_miio._udp.local.')
+
+    def info_from_entry(self, entry):
+        """Return most important info from mDNS entries."""
+
+        device_type = "UNKNOWN"
+        if entry.name.startswith("yeelink-light-color1_"):
+            device_type = "rgb"
+        elif entry.name.startswith("yeelink-light-mono1_"):
+            device_type = "white"
+        else:
+            logging.warning("Unknown miio device found: %s", entry)
+
+        return {"host": self.ip_from_host(entry.server),
+                "port": entry.port,
+                "hostname": entry.server,
+                "device_type": device_type,
+                "properties": entry.properties}
+
+    def get_entries(self):
+        return self.find_by_device_name('yeelink-light-')
