08147c376a4cb8ac6fc53d4be662ee0a91d68fae
bazwilliams <barry@bjw.me.uk>
2017-01-15 11:21:36 +0000

    Fixed line length issue

--- netdisco/discoverables/openhome.py
+++ netdisco/discoverables/openhome.py
@@ -8,7 +8,8 @@ class Discoverable(SSDPDiscoverable):
 
     def info_from_entry(self, entry):
         """Return the most important info from a uPnP entry."""
-        return entry.description['device']['friendlyName'], entry.values['location']
+        return (entry.description['device']['friendlyName'], 
+                entry.values['location'])
 
     def get_entries(self):
         """Get all the Openhome compliant device uPnP entries."""
