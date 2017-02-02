d02c0b60fa16100e9eeba2db25f6e867e176494c
Krasimir Zhelev <krasimir.zhelev@fau.de>
2017-02-02 21:39:57 +0100

    Added discovers for Frontier Silicon devices (#88)
    * added frontier_silicon const
    
    * added frontier_silicon discoverable
    
    * fixed identation warning
    
    * - fixed: E302 expected 2 blank lines, found 1

---
+++ netdisco/discoverables/frontier_silicon.py
@@ -0,0 +1,12 @@
+"""Discover frontier silicon devices."""
+from . import SSDPDiscoverable
+
+
+class Discoverable(SSDPDiscoverable):
+    """Add support for discovering frontier silicon devices."""
+
+    def get_entries(self):
+        """Get all the frontier silicon uPnP entries."""
+        return [entry for entry in self.netdis.ssdp.all()
+                if 'fsapi' in entry.st and
+                'urn:schemas-frontier-silicon-com' in entry.st]
