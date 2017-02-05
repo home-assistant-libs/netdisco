6690b6de2ee005060faef99a3d02c390468f71dd
Johan Bloemberg <github@ijohan.nl>
2017-02-05 22:31:45 +0100

    Add Fluxled discovery.

---
+++ netdisco/flux_led.py
@@ -0,0 +1,107 @@
+"""Fluxled device discovery.
+
+Ideally this would be done using broadcast. But some fluxled
+devices do not respond well to broadcast. So instead we send
+a packet to every device in the network. This is the way the
+offical app 'Magic Home' does it as well.
+
+"""
+import socket
+import sys
+import threading
+from datetime import timedelta
+import ipaddress
+from netdisco.util import interface_networks
+import logging
+
+
+DISCOVERY_PORT = 48899
+DISCOVERY_PAYLOAD = "HF-A11ASSISTHREAD".encode('ascii')
+DISCOVERY_TIMEOUT = timedelta(seconds=5)
+
+log = logging.getLogger(__name__)
+
+
+class FluxLed(object):
+    """Base class to discover Fluxled devices."""
+
+    def __init__(self, networks=None):
+        """Initialize the flux_led discovery."""
+        self.entries = []
+        self._lock = threading.RLock()
+
+        # if not explicitly specified determine network from interfaces
+        if networks:
+            self.networks = networks
+        else:
+            self.networks = interface_networks()
+
+    def scan(self):
+        """Scan the network."""
+        with self._lock:
+            self.update()
+
+    def all(self):
+        """Scan and return all found entries."""
+        self.scan()
+        return self.entries
+
+    def update(self):
+        """Scan network for Fluxled devices."""
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind(('', DISCOVERY_PORT))
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.settimeout(DISCOVERY_TIMEOUT.seconds)
+
+        # send query to every device in every network connected to
+        for network in self.networks:
+            for ip in network.hosts():
+                try:
+                    sock.sendto(DISCOVERY_PAYLOAD, (str(ip), DISCOVERY_PORT))
+                except OSError as exc:
+                    log.debug('failed to send request', exc_info=exc)
+                    continue
+
+        # wait for responses
+        while True:
+            try:
+                data, addr = sock.recvfrom(64)
+            except socket.timeout:
+                # no (more) responses received
+                break
+
+            # skip our own outgoing packet
+            if data == DISCOVERY_PAYLOAD:
+                continue
+
+            # data = ip_address,id,model
+            data = data.decode('ascii').split(',')
+            if len(data) < 3:
+                continue
+
+            entry = tuple(data)
+
+            if entry not in self.entries:
+                self.entries.append(entry)
+
+        sock.close()
+
+
+def main():
+    """Test Tellstick discovery."""
+    from pprint import pprint
+
+    if len(sys.argv) >= 2:
+        networks = [ipaddress.IPv4Network(n) for n in sys.argv[1:]]
+    else:
+        networks = None
+
+    flux_led = FluxLed(networks=networks)
+    pprint("Scanning for FluxLed devices..")
+    flux_led.update()
+    pprint(flux_led.entries)
+
+
+if __name__ == "__main__":
+    main()
