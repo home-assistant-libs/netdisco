b13921193dab1da9b9fdd3f015a4d502d5c9c39e
Martins Grunskis <martins@grunskis.com>
2017-02-09 16:49:59 +0100

    Implement N-UPnP Philips Hub discovery

--- netdisco/discoverables/philips_hue.py
+++ netdisco/discoverables/philips_hue.py
@@ -13,8 +13,30 @@ class Discoverable(SSDPDiscoverable):
 
     def get_entries(self):
         """Get all the Hue bridge uPnP entries."""
+        nupnp_entries = self.netdis.phue.entries
+
         # Hub models for year 2012 and 2015
-        return self.find_by_device_description({
+        ssdp_entries = self.find_by_device_description({
             "manufacturer": "Royal Philips Electronics",
             "modelNumber": ["929000226503", "BSB002"]
         })
+
+        return self.merge_entries(nupnp_entries, ssdp_entries)
+
+    # pylint: disable=no-self-use
+    def merge_entries(self, nupnp_entries, ssdp_entries):
+        """Takes lists of device entries found using N-UPnP & SSDP lookups and
+        merges them making sure that same device is only discoverd once.
+
+        """
+        entries = nupnp_entries
+
+        for nupn_entry in entries:
+            for ssdp_entry in ssdp_entries:
+                url_base1 = nupn_entry.description['URLBase']
+                url_base2 = ssdp_entry.description['URLBase']
+
+                if url_base1 != url_base2:
+                    entries.append(ssdp_entry)
+
+        return entries
