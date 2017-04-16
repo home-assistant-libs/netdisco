a42daedf456bb35f50fa53b9e178e8428df793d0
thrawnarn <thrawn@jungfrubo.se>
2017-04-16 12:27:44 +0200

    Added discovery support for the Bluesound platform

---
+++ netdisco/discoverables/bluesound.py
@@ -0,0 +1,11 @@
+"""Discover devices that implement the Bluesound platform."""
+from . import MDNSDiscoverable
+
+
+# pylint: disable=too-few-public-methods
+class Discoverable(MDNSDiscoverable):
+    """Add support for discovering Bluesound service."""
+
+    def __init__(self, nd):
+        """Initialize the Bluesound discovery."""
+        super(Discoverable, self).__init__(nd, '_musc._tcp.local.')
