2876d018c8594edc9eec257d1c0af016b5641eea
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-04-18 20:33:55 -0700

    Version bump to 1.0.0rc3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='netdisco',
-      version='1.0.0.rc2',
+      version='1.0.0.rc3',
       description='Discover devices on your local network',
       url='https://github.com/home-assistant/netdisco',
       author='Paulus Schoutsen',
