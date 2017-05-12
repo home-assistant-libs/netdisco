2b4eb211e8fba87c3a16a20dd1d213f986c2c6b0
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-05-12 08:53:23 -0700

    Remove Philips Hue nupnp (#121)
    * Remove Philips Hue nupnp
    
    * Make ssdp match_device_description only match each url once

--- netdisco/discoverables/philips_hue.py
+++ netdisco/discoverables/philips_hue.py
@@ -7,30 +7,7 @@ class Discoverable(SSDPDiscoverable):
 
     def get_entries(self):
         """Get all the Hue bridge uPnP entries."""
-        nupnp_entries = self.netdis.phue.entries
-
-        # Hub models for year 2012 and 2015
-        ssdp_entries = self.find_by_device_description({
+        return self.find_by_device_description({
             "manufacturer": "Royal Philips Electronics",
             "modelNumber": ["929000226503", "BSB002"]
         })
-
-        return self.merge_entries(nupnp_entries, ssdp_entries)
-
-    # pylint: disable=no-self-use
-    def merge_entries(self, nupnp_entries, ssdp_entries):
-        """Takes lists of device entries found using N-UPnP & SSDP lookups and
-        merges them making sure that same device is only discoverd once.
-
-        """
-        entries = {
-            nupn_entry.description['URLBase']: nupn_entry
-            for nupn_entry in nupnp_entries
-        }
-
-        for ssdp_entry in ssdp_entries:
-            url_base = ssdp_entry.description['URLBase']
-            if url_base not in entries:
-                entries[url_base] = ssdp_entry
-
-        return list(entries.values())
