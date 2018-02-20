f2604a01a09a26fa085a9791acc03ffb74d12514
Ville Skyttä <ville.skytta@iki.fi>
2018-02-20 02:15:14 +0200

    Add discovery of Huawei routers (#175)

--- netdisco/const.py
+++ netdisco/const.py
@@ -7,6 +7,7 @@ PHILIPS_HUE = "philips_hue"
 PMS = 'plex_mediaserver'
 LMS = 'logitech_mediaserver'
 ASUS_ROUTER = "asus_router"
+HUAWEI_ROUTER = "huawei_router"
 NETGEAR_ROUTER = "netgear_router"
 SONOS = "sonos"
 PANASONIC_VIERA = "panasonic_viera"
