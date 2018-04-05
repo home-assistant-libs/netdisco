6cfea4f43b9d477499a17e5a220e1e8ec14e4d5a
Sebastian Muszynski <s.muszynski@constructiva.de>
2018-04-05 19:18:44 +0200

    Pass the device name to the component

--- netdisco/discoverables/yeelight.py
+++ netdisco/discoverables/yeelight.py
@@ -3,6 +3,8 @@ import logging
 from . import MDNSDiscoverable
 from ..const import ATTR_DEVICE_TYPE
 
+DEVICE_NAME_PREFIX = 'yeelink-light-'
+
 
 # pylint: disable=too-few-public-methods
 class Discoverable(MDNSDiscoverable):
@@ -16,27 +18,12 @@ class Discoverable(MDNSDiscoverable):
         """Return most important info from mDNS entries."""
         info = super().info_from_entry(entry)
 
-        device_type = "UNKNOWN"
-        if entry.name.startswith("yeelink-light-color1_"):
-            device_type = "rgb"
-        elif entry.name.startswith("yeelink-light-mono1_"):
-            device_type = "white"
-        elif entry.name.startswith("yeelink-light-strip1_"):
-            device_type = "strip"
-        elif entry.name.startswith("yeelink-light-bslamp1_"):
-            device_type = "bedside"
-        elif entry.name.startswith("yeelink-light-ceiling1_"):
-            device_type = "ceiling"
-        elif entry.name.startswith("yeelink-light-ceiling2_"):
-            device_type = "ceiling2"
-        elif entry.name.startswith("yeelink-light-ceiling4_"):
-            device_type = "ceiling4"
-        else:
-            logging.warning("Unknown miio device found: %s", entry)
+        # Example name: yeelink-light-ceiling4_mibt72799069._miio._udp.local.
+        info[ATTR_DEVICE_TYPE] = \
+            entry.name.replace(DEVICE_NAME_PREFIX, '').rsplit('_', 1)[0]
 
-        info[ATTR_DEVICE_TYPE] = device_type
         return info
 
     def get_entries(self):
         """ Return yeelight devices. """
-        return self.find_by_device_name('yeelink-light-')
+        return self.find_by_device_name(DEVICE_NAME_PREFIX)
