b47dc3476bdc38bfa12ef5566cdac06f8c7b97c1
Sebastian Muszynski <s.muszynski@constructiva.de>
2018-04-05 19:29:58 +0200

    Keep the old mapping for stable entity ids

--- netdisco/discoverables/yeelight.py
+++ netdisco/discoverables/yeelight.py
@@ -19,9 +19,23 @@ class Discoverable(MDNSDiscoverable):
         info = super().info_from_entry(entry)
 
         # Example name: yeelink-light-ceiling4_mibt72799069._miio._udp.local.
-        info[ATTR_DEVICE_TYPE] = \
+        if entry.name.startswith("yeelink-light-color1_"):
+            device_type = "rgb"
+        elif entry.name.startswith("yeelink-light-mono1_"):
+            device_type = "white"
+        elif entry.name.startswith("yeelink-light-strip1_"):
+            device_type = "strip"
+        elif entry.name.startswith("yeelink-light-bslamp1_"):
+            device_type = "bedside"
+        elif entry.name.startswith("yeelink-light-ceiling1_"):
+            device_type = "ceiling"
+        elif entry.name.startswith("yeelink-light-ceiling2_"):
+            device_type = "ceiling2"
+        else:
+            device_type = \
             entry.name.replace(DEVICE_NAME_PREFIX, '').rsplit('_', 1)[0]
 
+        info[ATTR_DEVICE_TYPE] = device_type
         return info
 
     def get_entries(self):
