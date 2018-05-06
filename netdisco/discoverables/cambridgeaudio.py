f4c96e4c23bdd93b6f11bd8d1a0559f5c619ebb2
sebk-666 <seb.kaps@zoho.com>
2018-05-06 13:18:29 +0200

    Fixed comment refering to different document.

--- netdisco/discoverables/cambridgeaudio.py
+++ netdisco/discoverables/cambridgeaudio.py
@@ -7,7 +7,7 @@ class Discoverable(SSDPDiscoverable):
     """Add support for discovering Cambridge Audio StreamMagic devices."""
 
     def get_entries(self):
-        """Get all Denon AVR uPnP entries."""
+        """Get all Cambridge Audio MediaRenderer uPnP entries."""
         return self.find_by_device_description({
             "manufacturer": "Cambridge Audio",
             "deviceType": "urn:schemas-upnp-org:device:MediaRenderer:1"
