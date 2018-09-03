7c0e48f2782a9976f2d121a25f425e81bf5b31e7
Steven Looman <steven.looman@gmail.com>
2018-09-03 21:10:34 +0200

    Add UPnP/IGD discoverable (#210)

---
+++ netdisco/discoverables/igd.py
@@ -0,0 +1,14 @@
+"""Discover DLNA services."""
+from . import SSDPDiscoverable
+
+
+class Discoverable(SSDPDiscoverable):
+    """Add support for discovering IGD services."""
+
+    def get_entries(self):
+        """Get all the IGD service uPnP entries."""
+        return \
+            self.find_by_st(
+                "urn:schemas-upnp-org:device:InternetGatewayDevice:1") + \
+            self.find_by_st(
+                "urn:schemas-upnp-org:device:InternetGatewayDevice:2")
