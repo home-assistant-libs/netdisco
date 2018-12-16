7b8565665e279e5a1027f30c626bdd8d5d5587ef
Martin Hjelmare <marhje52@kth.se>
2018-12-16 17:21:38 +0100

    Update netdisco/discoverables/esphomelib.py
    Co-Authored-By: OttoWinter <otto@otto-winter.com>

--- netdisco/discoverables/esphomelib.py
+++ netdisco/discoverables/esphomelib.py
@@ -6,4 +6,4 @@ class Discoverable(MDNSDiscoverable):
     """Add support for discovering esphomelib devices."""
 
     def __init__(self, nd):
-        super(Discoverable, self).__init__(nd, '_esphomelib._tcp.local.')
+        super().__init__(nd, '_esphomelib._tcp.local.')
