530cb984d4e8378c70401e5666a7e459d8e83e12
Finbarr Brady <fbradyirl@users.noreply.github.com>
2019-02-28 22:39:42 +0000

    Create enigma2.py

---
+++ netdisco/discoverables/enigma2.py
@@ -0,0 +1,13 @@
+"""Discover Enigma2 servers."""
+from . import MDNSDiscoverable
+
+
+class Discoverable(MDNSDiscoverable):
+    """Add support for discovering Enigma2 boxes."""
+
+    def __init__(self, nd):
+        """Initialize the Enigma2 discovery."""
+        super(Discoverable, self).__init__(nd, '_e2stream._tcp.local.')
+
+    def get_entries(self):
+        return self.services.values()
