814a5c8b8ce4471c096484d13e1f57efff870dbc
Finbarr Brady <fibrady@cisco.com>
2019-03-05 21:27:31 +0000

    add discover for hikvision

---
+++ netdisco/discoverables/hikvision.py
@@ -0,0 +1,13 @@
+"""Discover SABnzbd servers."""
+from . import MDNSDiscoverable
+
+
+class Discoverable(MDNSDiscoverable):
+    """Add support for discovering SABnzbd."""
+
+    def __init__(self, nd):
+        """Initialize the SABnzbd discovery."""
+        super(Discoverable, self).__init__(nd, '_http._tcp.local.')
+
+    def get_entries(self):
+        return self.find_by_device_name('HIKVISION')
