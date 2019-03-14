5c2b710653b81b222dca786a10c9a79186571419
Paulus Schoutsen <balloob@gmail.com>
2019-03-13 21:33:20 -0700

    Update hass_mobile_app.py

--- netdisco/discoverables/hass_mobile_app.py
+++ netdisco/discoverables/hass_mobile_app.py
@@ -3,7 +3,7 @@ from . import MDNSDiscoverable
 
 
 class Discoverable(MDNSDiscoverable):
-    """Add support for discovering Home Assistant instances."""
+    """Add support for discovering mobile apps that support Home Assistant."""
 
     def __init__(self, nd):
         super(Discoverable, self).__init__(nd, '_hass-mobile-app._tcp.local.')
