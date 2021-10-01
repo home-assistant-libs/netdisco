477db5a1dc93919a6c5bd61b4b1d3c80e75785bd
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2021-10-01 09:16:55 -0700

    Drop discoverables that are unused (#257)

--- netdisco/discoverables/hass_ios.py
+++
@@ -1,9 +0,0 @@
-"""Discover Home Assistant iOS app."""
-from . import MDNSDiscoverable
-
-
-class Discoverable(MDNSDiscoverable):
-    """Add support for discovering the Home Assistant iOS app."""
-
-    def __init__(self, nd):
-        super(Discoverable, self).__init__(nd, '_hass-ios._tcp.local.')
